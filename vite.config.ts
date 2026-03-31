aaf393b2a2755cf395fae9ee4c25ae619608c3d8
Ian Pichardo <ian.pichardo@hotmail.com>
2026-03-31 19:06:53 -0400

    conf: Updated configuration for TS and vite, and updated index.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
+  resolve: {
+    alias: {
+      '$': path.resolve(__dirname, './src')
+    }
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
